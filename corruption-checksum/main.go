ca538178b3a438a5edf7c602c52751c0d7d6ab5f
Justin Tout <3639630+justintout@users.noreply.github.com>
2017-12-05 00:40:46 -0500

    finish part 2
    this was fun. more parallelism to go fast.
    
    main: kick off a goroutine for each row that calls divieRows. increment
    our waitgroup. kick off another goroutine to watch the waitgroup and close
    our result channel once we have all our results. pull from the prods
    channel, adding each integer to sum as it arrives.
    
    fan out: for each row, make a new channel to wait for the single result we
    expect. for each element in the row, pass of the copy of that row with
    the element removed along with our channel and the element itself to a
    goroutine that will check if that element is divisible by any of the
    memebrs of the new row. if it is, push the result of that division to the
    channel and close the channel. one we receive the single result from the
    channel, pass it along to the main's result channel.

--- corruption-checksum/main.go
+++ corruption-checksum/main.go
@@ -25,6 +25,21 @@ func main() {
 	if err != nil {
 		log.Fatalf("csv reader failed to read file: %v\n", err)
 	}
+
+	fmt.Printf("FILE CHECKSUM: %d\n", partOne(data))
+	fmt.Printf("DIVISION SUM: %d\n", partTwo(data))
+
+}
+
+func forceInt(s string) (i int) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("unable to convert %s to int: %v", s, err)
+	}
+	return
+}
+
+func partOne(data [][]string) int {
 	sum := 0
 	diffs := make(chan int)
 	var wg sync.WaitGroup
@@ -53,13 +68,47 @@ func main() {
 	for i := range diffs {
 		sum += i
 	}
-	fmt.Printf("INPUT CHECKSUM: %d\n", sum)
+	return sum
 }
 
-func forceInt(s string) (i int) {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Fatalf("unable to convert %s to int: %v", s, err)
+func partTwo(data [][]string) int {
+	prods := make(chan int)
+	var wg sync.WaitGroup
+	for _, row := range data {
+		// fmt.Println(row)
+		go divideRow(prods, &wg, row)
+		wg.Add(1)
 	}
-	return
+
+	go func(c chan int, wg *sync.WaitGroup) {
+		wg.Wait()
+		close(c)
+	}(prods, &wg)
+
+	sum := 0
+	for i := range prods {
+		sum += i
+	}
+	return sum
+}
+
+func divideRow(res chan int, wg *sync.WaitGroup, row []string) {
+	defer wg.Done()
+	c := make(chan int)
+	for i := 0; i < len(row); i++ {
+		var r []string
+		r = append(r, row[:i]...)
+		r = append(r, row[i+1:]...)
+		go func(c chan int, e string, r []string) {
+			i := forceInt(e)
+			for _, re := range r {
+				if i%forceInt(re) == 0 {
+					c <- i / forceInt(re)
+					close(c)
+				}
+			}
+		}(c, row[i], r)
+	}
+	result := <-c
+	res <- result
 }
