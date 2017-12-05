9ff1211a874cac4c5bed242cc67454f5a6d099c5
Justin Tout <3639630+justintout@users.noreply.github.com>
2017-12-04 23:36:21 -0500

    finish day 1
    so the "read from input.txt" style i'm gonig with is still rough. i
    decided to add a general "read this list of files and die if you cant"
    function to a AoC utility package (hiss utility package, i know)
    
    i switched gears a few times here. used text.scanner for a second, then
    read the raw bytes and split on newlnie or tab, ended up using the basic
    csv parser.
    
    superfluous parallelism probably, but gotta go for those style points.

--- corruption-checksum/main.go
+++ corruption-checksum/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
-
-	"github.com/justintout/advent-of-code/aocutil"
+	"strconv"
+	"sync"
 )
 
 func main() {
@@ -12,7 +14,52 @@ func main() {
 		fmt.Fprintf(os.Stderr, "usage:\n\t%s [input file]\n", os.Args[0])
 		os.Exit(1)
 	}
-	ins := aocutil.ReadInput(os.Args[1])
-	in := ins["input.txt"]
-	fmt.Printf("%s\n", in)
+	f, err := os.Open(os.Args[1])
+	defer f.Close()
+	if err != nil {
+		log.Fatalf("unable to read %s: %v\n", os.Args[1], err)
+	}
+	reader := csv.NewReader(f)
+	reader.Comma = '\t'
+	data, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalf("csv reader failed to read file: %v\n", err)
+	}
+	sum := 0
+	diffs := make(chan int)
+	var wg sync.WaitGroup
+	for _, row := range data {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, diffs chan int, strings []string) {
+			max := forceInt(strings[0])
+			min := forceInt(strings[0])
+			for _, s := range strings {
+				i := forceInt(s)
+				if i > max {
+					max = i
+				}
+				if i < min {
+					min = i
+				}
+			}
+			diffs <- max - min
+			wg.Done()
+		}(&wg, diffs, row)
+	}
+	go func(wg *sync.WaitGroup, c chan int) {
+		wg.Wait()
+		close(c)
+	}(&wg, diffs)
+	for i := range diffs {
+		sum += i
+	}
+	fmt.Printf("INPUT CHECKSUM: %d\n", sum)
+}
+
+func forceInt(s string) (i int) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("unable to convert %s to int: %v", s, err)
+	}
+	return
 }
